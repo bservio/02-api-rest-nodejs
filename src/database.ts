219d943de90dd1b76c9aad255abbbf769766994f
Bruno Sérvio Barbosa <brunoservio21@gmail.com>
2023-07-27 18:24:31 -0300

    feat: treating enviroment variables with zod

--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,5 @@
 import { knex as knexSetup, Knex } from 'knex'
+import { env } from './env'
 
 export const config: Knex.Config = {
   client: 'sqlite',
@@ -8,7 +9,7 @@ export const config: Knex.Config = {
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
-    directory: './db/migrations',
+    directory: env.DATABASE_URL,
   },
 }
 
